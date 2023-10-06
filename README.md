df4f6347d4734c2e0e07445831af5e63e38bd0bd
yong seock <myongdol132@gmail.com>
2023-10-06 18:21:49 +0900

    boxShadow 스타일링

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-메모
+## 블로그 메모 링크
+[boxShadow 스타일링](https://blog.naver.com/ares132/223229986752)  
--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -1,14 +1,32 @@
-import { TextInput, View } from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function GameStartScreen() {
     return (
-        <View>
+        <View style={styles.inputContainer}>
             <TextInput />
-            <PrimaryButton>초기화</PrimaryButton>
             <PrimaryButton>확인</PrimaryButton>
+            <PrimaryButton>초기화</PrimaryButton>
         </View>
     )
 };
 
 export default GameStartScreen;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        padding: 16,
+        marginTop: 100,
+        marginHorizontal: 24,
+        backgroundColor: '#72063c',
+        borderRadius: 12,
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: {
+            width: 0,
+            height: 2,
+        },
+        shadowRadius: 6,
+        shadowOpacity: 0.25,
+    }
+});
