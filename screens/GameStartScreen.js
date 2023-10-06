11d0ec05c1780e35bba5fc5f512886894429e516
yong seock <myongdol132@gmail.com>
2023-10-06 18:27:32 +0900

    textInput 필드 구성

--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function GameStartScreen() {
     return (
         <View style={styles.inputContainer}>
-            <TextInput style={styles.numberInput} maxLength={2}/>
+            <TextInput 
+                style={styles.numberInput} 
+                maxLength={2} 
+                keyboardType="number-pad"
+                autoCapitalize="none"
+                autoCorrect={false}
+            />
             <PrimaryButton>확인</PrimaryButton>
             <PrimaryButton>초기화</PrimaryButton>
         </View>
