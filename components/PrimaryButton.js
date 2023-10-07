fd4ada5cd8572e2ecedf03c49b39c1c2b1713710
yong seock <myongdol132@gmail.com>
2023-10-07 23:21:59 +0900

    버튼에 시각적 피드백 추가

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,13 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, Pressable, StyleSheet } from 'react-native'
 
 function PrimaryButton({children}) {
+    function pressHandler() {
+        console.log('터치')
+    }
     return (
-        <View>
-            <Text>
+        <View style={styles.buttonOuterContainer}>
+        <Pressable 
+          onPress={pressHandler}
+          android_ripple={{color: '#640233'}}
+          style={({pressed}) => pressed ? [styles.buttonInnerContainer, styles.pressed] : styles.buttonInnerContainer}
+        >
+            <Text style={styles.buttonText}>
                 {children}
             </Text>
+        </Pressable>
         </View>
     )
 };
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+    buttonInnerContainer: {
+        backgroundColor: '#72063c',
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        elevation: 2, // 안드로이드 전용 속성
+    },
+    buttonOuterContainer: {
+        borderRadius: 32,
+        margin: 4,
+        overflow: 'hidden'
+    },
+    buttonText: {
+        color: 'white',
+        textAlign: 'center',
+    },
+    pressed: {  // iOS 스타일링
+        opacity: 0.75,
+    }
+});
