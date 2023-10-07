c149f4cd332bd9c2a79e2ca3fd3aca73d9f25635
yong seock <myongdol132@gmail.com>
2023-10-07 23:42:17 +0900

    선형 그라데이션 추가

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import GameStartScreen from './screens/GameStartScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <GameStartScreen />
+    <LinearGradient 
+      style={styles.mainScreen}
+      colors={['#4e0329', '#ddb52f']}
+    >
+      <GameStartScreen />
+    </LinearGradient>
   );
 }
 
 const styles = StyleSheet.create({
- 
+ mainScreen: {
+  flex: 1,
+ }
 });
