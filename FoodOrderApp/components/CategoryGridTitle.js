ce42166706e3be55899cddc71c8ef09cedef9b00
yong seock <myongdol132@gmail.com>
2023-10-14 20:44:55 +0900

    카테고리 출력

---
+++ FoodOrderApp/components/CategoryGridTitle.js
@@ -0,0 +1,17 @@
+import { Pressable, Text, View } from "react-native";
+
+function CategoryGridTitle({title, color}) {
+    return (
+        <View>
+            <Pressable>
+                <View>
+                    <Text>
+                        {title}
+                    </Text>
+                </View>
+            </Pressable>
+        </View>
+    )
+};
+
+export default CategoryGridTitle;
---
+++ FoodOrderApp/screens/CategoriesScreen.js
@@ -0,0 +1,23 @@
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data'
+import CategoryGridTitle from '../components/CategoryGridTitle';
+
+function renderItem(itemData) {
+    return (
+        <CategoryGridTitle
+            title={itemData.item.title}
+            color={itemData.item.color}
+        />
+    )
+};
+
+function CartegoriesScreen() {
+    return (
+        <FlatList 
+          data={CATEGORIES}
+          keyExtractor={(item) => item.id}
+          renderItem={renderItem}
+        />
+    )
+};
+export default CartegoriesScreen;
