d9597e98372ba5234dc7c73544cb6fd193fbefcd
yong seock <myongdol132@gmail.com>
2023-10-18 03:00:47 +0900

    Fix::패키지 전부 강의버전에 맞게 재설치

--- FoodOrderApp/node_modules/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/MapUtils.java
+++ FoodOrderApp/node_modules/react-native-reanimated/android/src/main/java/com/swmansion/reanimated/MapUtils.java
@@ -3,11 +3,13 @@ package com.swmansion.reanimated;
 import com.facebook.react.bridge.JSApplicationCausedNativeException;
 import com.facebook.react.bridge.NoSuchKeyException;
 import com.facebook.react.bridge.ReadableMap;
+import com.facebook.react.bridge.ReadableType;
+
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 
 public class MapUtils {
-  public static int getInt(ReadableMap map, @Nonnull String name, String errorMsg) {
+  public static int getInt(ReadableMap map,  @Nonnull String name, String errorMsg) {
     try {
       return map.getInt(name);
     } catch (NoSuchKeyException e) {
@@ -16,7 +18,7 @@ public class MapUtils {
   }
 
   @Nullable
-  public static String getString(ReadableMap map, @Nonnull String name, String errorMsg) {
+  public static String getString(ReadableMap map,  @Nonnull String name, String errorMsg) {
     try {
       return map.getString(name);
     } catch (NoSuchKeyException e) {
