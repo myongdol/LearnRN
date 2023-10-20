a0a47d0e4da7f3b363ea71ce34183fe2b746b854
yong seock <myongdol132@gmail.com>
2023-10-20 20:43:23 +0900

    Feat::네비게이션 구성

--- Household ledger/App.js
+++ Household ledger/App.js
@@ -1,20 +1,34 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import ManageExpense from './screens/ManageExpense';
+import RecentExpenses from './screens/RecentExpenses';
+import AllExpenses from './screens/AllExpenses';
+
+
+const STACK = createNativeStackNavigator();
+const BOTTOM_TAB = createBottomTabNavigator();
+
+function ExpensesOverview() {
+  return (
+  <BOTTOM_TAB.Navigator>
+    <BOTTOM_TAB.Screen name="지출내용" component={RecentExpenses} />
+    <BOTTOM_TAB.Screen name="전체비용" component={AllExpenses} />
+  </BOTTOM_TAB.Navigator>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>헬로우!! </Text>
+    <>
       <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <STACK.Navigator>
+          <STACK.Screen name="전체지출" component={ExpensesOverview} />
+          <STACK.Screen name="비용관리" component={ManageExpense} />
+        </STACK.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
