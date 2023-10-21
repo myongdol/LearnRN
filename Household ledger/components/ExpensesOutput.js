6722f41bb359e45c8c361481e22cc8f2ffd94af4
yong seock <myongdol132@gmail.com>
2023-10-21 23:06:26 +0900

    Design::레이아웃 스타일링

--- Household ledger/components/ExpensesOutput.js
+++ Household ledger/components/ExpensesOutput.js
@@ -1,6 +1,7 @@
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import ExpensesSummary from "./ExpensesSummary";
 import ExpensesList from "./ExpensesList";
+import { GlobalStyles } from "../constants/styles";
 
 
 const DUMMY_EXPENSES = [
@@ -39,7 +40,7 @@ const DUMMY_EXPENSES = [
 
 function ExpensesOutput({ expenses, expensesPeriod }) {
     return (
-        <View>
+        <View style={styles.container}>
             <ExpensesSummary 
                 expenses={DUMMY_EXPENSES}
                 periodName={expensesPeriod}
@@ -52,3 +53,11 @@ function ExpensesOutput({ expenses, expensesPeriod }) {
 };
 
 export default ExpensesOutput;
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary700,
+        flex: 1,
+    }
+})
