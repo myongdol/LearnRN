0064cd3e5533acbaaafd0df484a0bb6404cd6b2d
yong seock <myongdol132@gmail.com>
2023-10-21 23:29:13 +0900

    Design::날짜형식 지정

--- Household ledger/components/ExpenseItem.js
+++ Household ledger/components/ExpenseItem.js
@@ -1,5 +1,6 @@
 import { Pressable, StyleSheet, Text, View } from "react-native";
 import { GlobalStyles } from "../constants/styles";
+import { getFormattedDate } from "../util/date";
 
 
 function ExpenseItem({description, amount, date}){
@@ -8,10 +9,10 @@ function ExpenseItem({description, amount, date}){
             <View style={styles.expenseItem}>
                 <View>
                     <Text style={[styles.textBase, styles.description]}>{description}</Text>
-                    <Text style={styles.textBase}>{date.toString()}</Text>
+                    <Text style={styles.textBase}>{getFormattedDate(date)}</Text>
                 </View>
                 <View style={styles.amountContainer}>
-                    <Text style={styles.amount}>{amount}</Text>
+                    <Text style={styles.amount}>{amount.toFixed(2)}</Text>
                 </View>
             </View>
         </Pressable>
@@ -48,7 +49,8 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         justifyContent: 'center',
         alignItems: 'center',
-        borderRadius: 4
+        borderRadius: 4,
+        minWidth: 90,
     },
     amount: {
         color: GlobalStyles.colors.primary500,
--- Household ledger/components/ExpensesOutput.js
+++ Household ledger/components/ExpensesOutput.js
@@ -35,6 +35,36 @@ const DUMMY_EXPENSES = [
         amount: 2100,
         date: new Date('2023-12-09')
     },
+        {
+        id: 'e6',
+        description: '후드티',
+        amount: 500,
+        date: new Date('2023-10-21')
+    },
+    {
+        id: 'e7',
+        description: '반팔티',
+        amount: 800,
+        date: new Date('2023-11-22')
+    },
+    {
+        id: 'e8',
+        description: '버블티',
+        amount: 300.2,
+        date: new Date('2022-10-21')
+    },
+    {
+        id: 'e9',
+        description: '프론트엔드 강의',
+        amount: 1200,
+        date: new Date('2023-12-21')
+    },
+    {
+        id: 'e10',
+        description: '취업하고싶다',
+        amount: 2100,
+        date: new Date('2023-12-09')
+    },
 ]
 
 
@@ -56,7 +86,8 @@ export default ExpensesOutput;
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24,
+        paddingHorizontal: 24,
+        paddingTop: 0,
         backgroundColor: GlobalStyles.colors.primary700,
         flex: 1,
     }
