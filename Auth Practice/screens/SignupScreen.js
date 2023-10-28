a3790581daab8f79a79ba1e61a7a93e3ace855ca
yong seock <myongdol132@gmail.com>
2023-10-28 23:49:51 +0900

    Feat::firebase 회원가입 기능구현

--- Auth Practice/screens/SignupScreen.js
+++ Auth Practice/screens/SignupScreen.js
@@ -1,7 +1,22 @@
+import { useState } from 'react';
 import AuthContent from '../components/Auth/AuthContent';
+import { createUser } from '../util/auth';
+import LoadingOverlay from '../components/ui/LoadingOverlay';
 
 function SignupScreen() {
-  return <AuthContent />;
+  const [isAuth, setIsAuth] = useState(false);
+
+  async function signupHandler({email, password}) {
+    setIsAuth(true);
+    await createUser(email, password);
+    setIsAuth(false);
+  };
+
+  if (isAuth) {
+    return <LoadingOverlay message={"회원가입 중.."} />
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
 }
 
 export default SignupScreen;
--- Auth Practice/util/auth.js
+++ Auth Practice/util/auth.js
@@ -1,14 +1,20 @@
-import { Axios } from "axios";
+import axios from "axios";
 
 const API_KEY = 'AIzaSyCOF2EQSxJLYgeSw23AGwMGgN7srydD5gY';
 
-async function createUser(email, password) {
-    const RESPONSE = await Axios.post(
-        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=' + API_KEY,
-        {
-            email: email,
-            password: password,
-            returnSecureToken: true,
-        }
-    );
-};
+export async function createUser(email, password) {
+    try {
+        const response = await axios.post(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + API_KEY,
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true,
+            }
+        );
+        return response.data;
+    } catch (error) {
+        console.error("Error creating user:", error.response ? error.response.data : error.message);
+        throw error;
+    }
+}
