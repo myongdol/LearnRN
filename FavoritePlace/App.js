4b65519cbc8aed62ad7a846cc055010f6cf120c1
yong seock <myongdol132@gmail.com>
2023-10-30 22:11:23 +0900

    Feat: 헤더에 장소추가 아이콘 버튼 추가 및 IconButton컴포넌트 구현
    - App.js에서 Allplace 스크린의 헤더 우측에 아이콘 버튼을 추가하였습니다.
    - 새로운 IconButton.js 컴포넌트를 구현 하였습니다.
     + 주어진 아이콘, 크기, 색상으로 버튼을 표시 합니다.
     + 클릭시 전달된 onPress 이벤트가 호출됩니다.
     + 버튼이 클릭되면 투명도를 조절하여 스타일을 변경합니다.

--- FavoritePlace/App.js
+++ FavoritePlace/App.js
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
+import IconButton from './components/UI/IconButton';
 
 
 const STACK = createNativeStackNavigator();
@@ -13,7 +14,20 @@ export default function App() {
       <StatusBar style='dark' />
       <NavigationContainer>
         <STACK.Navigator>
-          <STACK.Screen name="모든장소" component={AllPlaces} />
+          <STACK.Screen 
+            name="모든장소" 
+            component={AllPlaces} 
+            options={({navigation}) => ({
+              headerRight: ({tintColor}) => (
+              <IconButton 
+                color={tintColor}
+                size={24}
+                icon="add"
+                onPress={() => navigation.navigate('장소추가') }
+              />
+              )
+          })}
+          />
           <STACK.Screen name="장소추가" component={AddPlace} />
         </STACK.Navigator>
       </NavigationContainer>
