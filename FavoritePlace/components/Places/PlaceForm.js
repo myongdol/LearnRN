680cae11b02dc10aebb81ce9d7051cf20cc44876
yong seock <myongdol132@gmail.com>
2023-10-30 21:47:56 +0900

    Feat: 장소추가 화면 및 네비게이션 설정
    - @react-navigation/native와 @react-navigation/native-stack 패키지
       설치를 하였습니다.
    - PlaceForm을 이용하여 AddPlace 화면을 간단하게 구성 하였습니다.
    - AllPlace와 AddPlace 화면을 네비게이션 스택에 추가 하였습니다.

---
+++ FavoritePlace/components/Places/PlaceForm.js
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+
+
+function PlaceForm() {
+    return (
+        <View>
+            <Text>플레이스 폼</Text>
+        </View>
+    )
+};
+
+export default PlaceForm;
---
+++ FavoritePlace/screens/AddPlace.js
@@ -0,0 +1,10 @@
+import PlaceForm from "../components/Places/PlaceForm";
+
+
+function AddPlace() {
+    return (
+        <PlaceForm />
+    )
+};
+
+export default AddPlace;
