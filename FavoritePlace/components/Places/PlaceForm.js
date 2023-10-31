06aaabd63e486dfd23041143524bece0cd9b3c03
yong seock <myongdol132@gmail.com>
2023-10-31 20:25:02 +0900

    Feat: PlaceForm컴포넌트 추가
    - useState를 사용하여 사용자의 입력을 관리합니다.
    - TextInput 컴포넌트를 통해 사용자의 입력을 받아옵니다.
    - ScrollView를 적용하여 폼의 내용이 많을 경우 스크롤을 하여 볼수 있게
      합니다.

--- FavoritePlace/components/Places/PlaceForm.js
+++ FavoritePlace/components/Places/PlaceForm.js
@@ -1,12 +1,48 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+import { Colors } from "../../constants/colors";
 
 
 function PlaceForm() {
+    const [enteredTitle, setEnteredTitle] = useState('');
+
+    function changeTitleHandler(enteredText) {
+        setEnteredTitle(enteredText)
+    };
+
     return (
-        <View>
-            <Text>플레이스 폼</Text>
-        </View>
+        <ScrollView style={STYLES.form}> 
+             <View>
+                <Text style={STYLES.label}>제목</Text>
+                <TextInput 
+                    style={STYLES.input}
+                    onChange={changeTitleHandler}
+                    value={enteredTitle}
+                />
+             </View>
+        </ScrollView>
     )
 };
 
 export default PlaceForm;
+
+const STYLES = StyleSheet.create({
+    form: {
+        flex: 1,
+        padding: 24,
+    },
+    label: {
+        fontWeight: 'bold',
+        marginBottom: 4,
+        color: Colors.primary500,
+    },
+    input: {
+        marginVertical: 8,
+        paddingHorizontal: 4,
+        paddingVertical: 8,
+        fontSize: 16,
+        borderBottomColor: Colors.primary700,
+        borderBottomWidth: 2,
+        backgroundColor: Colors.primary100,
+    }
+});
