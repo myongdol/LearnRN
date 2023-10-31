8bb043a3c7f20aa5e760bfdf51c1661d5b6d8d62
yong seock <myongdol132@gmail.com>
2023-10-31 21:54:41 +0900

    Feat: 카메라 권한 요청 및 검사로직 추가
    - useCameraPermissions훅을 사용하여 카메라 접근권한의 상태를 관리하고
      필요한 경우 사용자에게 권한을 요청합니다.
    - 권한 상태에 따라 사용자에게 Alert를 제공합니다. 특히 권한이 없거나
      거부되었을 경우 사용자에게 알림창을 통해 안내 합니다.
    - 사용자가 카메라 접근권한을 부여하지 않았을 경우 사진찍기 버튼을
      클릭해도 카메라가 호출되지않습니다.

--- FavoritePlace/components/Places/ImagePicker.js
+++ FavoritePlace/components/Places/ImagePicker.js
@@ -1,10 +1,34 @@
-import { Button, View } from "react-native";
-import { launchCameraAsync } from 'expo-image-picker';
+import { Alert, Button, View } from "react-native";
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
 
 
 function ImagePicker() {
+    const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+
+    async function verifyPermissions() {
+        if (cameraPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+            const permissionResponse = await requestPermission();
+
+            return permissionResponse.granted;
+        };
+
+        if (cameraPermissionInformation.status === PermissionStatus.DENIED) {
+            Alert.alert('권한이 없습니다.', '권한요청 팝업이 뜨면 허용해주세요.');
+
+            return false;
+        };
+        
+        return true;
+    };
+
     async function takeImageHandler() {
-       const image = await launchCameraAsync({
+        const hasPermission = await verifyPermissions();
+
+        if(!hasPermission) {
+            return;
+        }
+
+        const image = await launchCameraAsync({
         allowsEditing: true,
         aspect: [16, 9], 
         quality: 0.5,
