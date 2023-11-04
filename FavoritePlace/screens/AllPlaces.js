5e7d6a7457324a1d573c7f3e78383607b251fdbb
yong seock <myongdol132@gmail.com>
2023-11-04 22:17:29 +0900

    Feat: AllPlaces 컴포넌트에 장소상태 및 네비게이션 포커스 관리 추가
    - useIsFocused 훅을 사용하여 현재 화면의 포커스 상태를 감지
    - 선택된 장소를 loadedPlaces 상태에 추가하여 관리
    - route.params를 통해 전달받은 새로운 장소 정보를 상태에 반영
    - PlacesList 컴포넌트에 loadedPlaces를 props로 전달하여 리스트 표시

--- FavoritePlace/screens/AllPlaces.js
+++ FavoritePlace/screens/AllPlaces.js
@@ -1,9 +1,19 @@
+import { useIsFocused } from "@react-navigation/native";
 import PlacesList from "../components/Places/PlacesList";
+import { useEffect, useState } from "react";
 
 
-//가장 먼저 보이는 화면
-function AllPlaces() {
-    return <PlacesList />
+
+function AllPlaces({route}) {
+    const [loadedPlaces, setLoadedPlaces] = useState([]);
+    const isFocused = useIsFocused();
+    useEffect(() => {
+        if (isFocused && route.params) {
+            setLoadedPlaces(curPlaces => [...curPlaces, route.params.place]);
+        }
+    }, [isFocused, route]);
+
+    return <PlacesList places={loadedPlaces}/>;
 };
 
 export default AllPlaces;
