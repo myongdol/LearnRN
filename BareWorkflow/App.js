692e6fb59aaae1820b41368efb8b5d67e36d65cf
yong seock <myongdol132@gmail.com>
2023-11-06 23:02:15 +0900

    Feat: expo-location패키지 설치하고 적용해보기
    - expo-location 패키지를 설치하고 ios, android환경에 대한 설정
    - App.js
     + 위치 정보에 대한 권한을 요청하는 getLocation 추가
     + 위치 권한이 허용되면 위치정보를 콘솔에 출력

--- BareWorkflow/App.js
+++ BareWorkflow/App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+  async function getLocation() {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    console.log(location);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button 
+        title="위치인증"
+        onPress={getLocation}
+      />
     </View>
   );
 }
---
+++ BareWorkflow/README.md
@@ -0,0 +1,4 @@
+## 블로그 메모링크
+
+[시작하기](https://blog.naver.com/ares132/223257558046)  
+[ERROR: JAVA_HOME is set to an invalid directory 에러해걸](https://blog.naver.com/ares132/223257466761)  
